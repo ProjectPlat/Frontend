f5e5e940d7e6c94bed9b848bbc1e79e9de81c288
SirCrewete <gimenocrew1996@gmail.com>
2022-06-05 12:30:43 +0200

    PLAT-11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import SampleContainer from './Components/SampleContainer/SampleContainer';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SampleContainer />
     </div>
   );
 }
---
+++ src/Components/SampleContainer/SampleContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SampleContainer.css'
+
+function SampleContainer () {
+    return (
+        <div className='sampleContainer'>
+            <h3>
+                Sample amazing text
+            </h3>
+        </div>
+    )
+}
+
+export default SampleContainer;
