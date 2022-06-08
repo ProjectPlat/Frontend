ff3b0136ad6705e0391ed2525c26ac673773bdab
SirCrewete <gimenocrew1996@gmail.com>
2022-06-08 18:11:13 +0200

    testing server

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import NavBar from './Components/NavBar/NavBar';
-
+import { v4 as uuid } from 'uuid';
 
 function App() {
+
+  const session_id = uuid();
+  console.log('session_id:', session_id);
+
   return (
-    <div className="App">
+    <div className="App"> 
       <NavBar />
       <Header />
     </div>
