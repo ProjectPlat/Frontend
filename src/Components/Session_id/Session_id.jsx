4f826429dfedabe37d76fbdeff412652848c17aa
SirCrewete <gimenocrew1996@gmail.com>
2022-06-09 20:45:44 +0200

    feature-PLAT-15 cookie expires ok

--- src/Components/Session_id/Session_id.jsx
+++ src/Components/Session_id/Session_id.jsx
@@ -4,7 +4,7 @@ import Cookies from 'universal-cookie';
 const session_id = uuid();
 console.log(session_id);
 
-const daysToExpire = new Date(2147483647 * 1000).toUTCString();
+const daysToExpire = new Date(2147483647 * 1000);
 
 const cookies = new Cookies();
 cookies.set('session_id', session_id, { path: '/', expires: daysToExpire });
