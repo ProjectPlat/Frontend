f2e39b462476e2d211650fa8c09a26e99c97bc77
SirCrewete <gimenocrew1996@gmail.com>
2022-06-11 09:55:33 +0200

    feature PLAT-14 sentry installed & error report test ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import * as Sentry from "@sentry/react";
 import Header from './Components/Header/Header';
 import NavBar from './Components/NavBar/NavBar';
 
@@ -8,10 +9,11 @@ function App() {
   return (
     <div className="App"> 
       <NavBar />
+        <button onClick={methodDoesNotExist}>Break the world</button>;
       <Header />
     </div>
   );
 }
 
 
-export default App;
+export default Sentry.withProfiler(App);
