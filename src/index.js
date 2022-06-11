5da9a00398abe044e9759833af776c52f0a82342
SirCrewete <gimenocrew1996@gmail.com>
2022-06-11 09:19:04 +0200

    CamelCase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './Services/Session_id/Session_id'
+import './Services/SessionId/SessionId'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
