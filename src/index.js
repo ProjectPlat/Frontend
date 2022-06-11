cc5664f4cebb1eb501508ce1827836ff14717f8a
SirCrewete <gimenocrew1996@gmail.com>
2022-06-11 09:55:33 +0200

    feature PLAT-14 sentry installed & error report test ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Services/SessionId/SessionId'
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://6e8802292d504ebe986c8771e8f9da16@o1276217.ingest.sentry.io/6495105",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
