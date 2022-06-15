014ed30df3d47fff24af977ee3ed372f5dbfefb5
Esteve Segura <esteve.segura@voicemod.net>
2022-06-15 16:23:39 +0200

    Improve sentry management

--- src/Services/SessionId/SessionId.js
+++ src/Services/SessionId/SessionId.js
@@ -1,23 +1,15 @@
 import Cookies from 'universal-cookie';
-import { generateId } from '../../Services/IdGenerator/IdGenerator';
+import { generateId } from '../idGenerator/idGenerator';
 import { MAX_DATE } from '../../Constants/constants';
 
 const session_id = generateId;
 
-var loggedIn = false;
-
 const daysToExpire = new Date(MAX_DATE);
 
 const cookies = new Cookies();
-
 const sessionIdExists = cookies.get('session_id')
 
 if(sessionIdExists) {
-    loggedIn = true;
-} else {
-    loggedIn = false;
     cookies.set('session_id', session_id, { path: '/', expires: daysToExpire }); 
+    localStorage.sessionId = session_id
 }
-
-
-
--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: "https://6e8802292d504ebe986c8771e8f9da16@o1276217.ingest.sentry.io/6495105",
   integrations: [new BrowserTracing()],
-
-  
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
   tracesSampleRate: 1.0,
+});
+
+const userSession = localStorage.getItem('sessionId');
+Sentry.setContext("User", {
+    loggedIn: false,
+    sessionId: userSession,
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -25,7 +26,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
