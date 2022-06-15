ba2a3d0ef6337853442a817fa84f370de8518eaa
SirCrewete <gimenocrew1996@gmail.com>
2022-06-15 21:33:52 +0200

    feature/PLAT-13 Testing Segment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import './Services/SessionId/SessionId'
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="MNpNGcIT8GgMOvqoKiFAYPvLJy54bU7s";;analytics.SNIPPET_VERSION="4.15.3";
+analytics.load("MNpNGcIT8GgMOvqoKiFAYPvLJy54bU7s");
+analytics.page();
+}}();
+
 Sentry.init({
   dsn: "https://6e8802292d504ebe986c8771e8f9da16@o1276217.ingest.sentry.io/6495105",
   integrations: [new BrowserTracing()],
